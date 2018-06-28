bf3242edc34aea7802fdcbdc0b03553a3b5a3939
asthas <asthamaths@gmail.com>
2018-06-29 00:31:04 +0530

    Normalize search term when filtering

--- src/utils/normalize-repos.js
+++ src/utils/normalize-repos.js
@@ -2,7 +2,7 @@ import sortBy from "./sort-by";
 
 const normalizeRepos = (repos, filter, sortProp) =>
   Object.values(repos)
-    .filter(repo => repo.name.toLowerCase().includes(filter))
+    .filter(repo => repo.name.toLowerCase().includes(filter.toLowerCase()))
     .sort(sortBy(sortProp))
     .reverse();
 
